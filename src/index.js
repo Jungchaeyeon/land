1852d10a931dba6e7d378dccd8ada776756963a9
Jungchaeyeon <chaeyeon1697@gmail.com>
2021-06-19 19:21:39 +0900

    init5

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import * as serviceWorker from './serviceWorker';
 import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.render(
-  <BrowserRouter basename="https://jungchaeyeon.github.io">
+  <BrowserRouter basename="https://jungchaeyeon.github.io/land">
   <App />
 </BrowserRouter>,
   document.getElementById('root')
