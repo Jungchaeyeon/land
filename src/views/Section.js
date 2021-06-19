fe6ebc025752ed340b7c504857d6d1ceab931fca
Jungchaeyeon <chaeyeon1697@gmail.com>
2021-06-19 19:03:24 +0900

    text

--- src/views/Section.js
+++ src/views/Section.js
@@ -10,8 +10,8 @@ function Section() {
         <div className='section'>
         
         <Switch>
-          <Route exact path="/" component={LandingPage} /> 
-          <Route exact path="/port" component={PortfolioPage} />{/* null - 누구나 접속 가능한 페이지 */}
+          <Route exact path="/main" component={LandingPage} /> 
+          <Route exact path="/portfolio" component={PortfolioPage} />{/* null - 누구나 접속 가능한 페이지 */}
         </Switch>
       </div>
     )
