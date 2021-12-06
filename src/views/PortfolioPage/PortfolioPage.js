1d31be85701ce21c5a0c863c895fa497c9503c9b
Jungchaeyeon <chaeyeon1697@gmail.com>
2021-12-06 21:20:51 +0900

    포트폴리오 수정 및 업데이트

--- src/views/PortfolioPage/PortfolioPage.js
+++ src/views/PortfolioPage/PortfolioPage.js
@@ -14,6 +14,86 @@ function PortfolioPage() {
     return (
         <section className="container">
             <form className="milliewillie">
+            <form className="appintro">
+                    <h1 style={{ color:"#faf1d0"}}>디저트 주문앱 </h1>
+                    <ul style={{ marginTop:'30px'}}>
+                        Mocking Data를 활용한 내 주변의 카페를 찾고, 주문할 수 있는 앱
+                    <br />
+                    <br /></ul>
+                
+                        <li>기술스택: MVVM -retrofit2, Coroutines, firebase, koin , GoogleMap, Glide, kotlin, room, viewBinding, liveData</li>
+                        <ol style={{ marginTop:'30px'}}>구현 기능: 
+                            <li className="func">내 위치를 기반으로 주위 카페 리스트 보여주기 (원하는 위치로 조정 가능)</li>
+                            <li className="func">찜 기능  </li>
+                            <li className="func">주문 기능</li>
+                            <li className="func">로그인 기능</li>
+                        </ol>
+                        <section style={{ marginTop: "30px"}}>
+                            <img src="dessert1.png" style={{height:'30%',marginRight:'15px' }}/>
+                            <img src="dessert2.png" style={{height:'30%', marginRight:'15px'}}/>
+                            <img src="dessert3.png" style={{height:'30%',marginRight:'15px'}}/>
+                            <img src="dessert4.png" style={{height:'30%',marginRight:'15px'}}/>
+                            <img src="dessert5.png" style={{height:'30%',marginRight:'15px'}}/>
+                            <br/>
+                            <a style={{ marginTop:'30px', marginBottom: '100px', color:"#b8f1d0"}} href="https://github.com/Jungchaeyeon/LetsGoHiking">github 프로젝트 바로가기</a>
+                        </section>
+                    </form>
+            <form className="appintro">
+                    <h1 style={{ color:"#faf1d0"}}>AI 기반 재활용 분리수거시스템 </h1>
+                    <ul style={{ marginTop:'30px'}}>
+                     폐기물 선별장에서의 열악한 노동처우와 기존 폐기물 처리 기술 개선을 위해 
+                     <br />인공지능을 활용해 재활용품을 자동선별하는 서비스
+                    <br />
+                       자체 구축한 모양 및 재질 분류 알고리즘을 통해 동일한 모양이더라도 다른 재질을 가지는
+                    <br/> 물체까지 분리할 수 있습니다. 실시간으로 처리되는 재활용품 데이터들을 웹/ 앱 서비스로 제공합니다.
+                    <br/> 또한 컨베이어벨트 이상 발생 시 관리자에게 즉각적인 알림을 전송할 수 있는 기능을 구현하였습니다.
+
+                    <br /></ul>
+                
+                        <li>기술스택: React-native, javascript, AWS, EC2, MongoDB</li>
+                        <ol style={{ marginTop:'30px'}}>구현 기능: 
+                            <li className="func">AWS의 우분투 가상머신에 서버 구현, 웹 서비스구현</li>
+                            <li className="func">Unity와 소켓통신을 통해 실시간 폐기물 처리 데이터를 DB에 저장 및 불러오는 기능 구현  </li>
+                            <li className="func">Web View를 앱에 띄울 수 있는 앱서비스 구현</li>
+                        </ol>
+                        <section style={{ marginTop: "30px"}}>
+                            <img src="trashcollection1.png" style={{height:'30%',marginRight:'15px' }}/>
+                            <img src="trashcollection2.png" style={{height:'30%', marginRight:'15px'}}/>
+                            <img src="trashcollection3.png" style={{height:'30%',marginRight:'15px'}}/>
+                            <br/>
+                            <a style={{ marginTop:'30px', marginBottom: '100px', color:"#b8f1d0"}} href="https://github.com/Jungchaeyeon/LetsGoHiking">github 프로젝트 바로가기</a>
+                        </section>
+                    </form>
+                <form className="appintro">
+                    <h1 style={{ color:"#faf1d0"}}>이번주 등산고? </h1>
+                    <ul style={{ marginTop:'30px'}}>
+                     산에 대한 정보와 산행포인트,등산로 정보, 더불어 지역별, 난이도별 산에 대한 정보를 제공합니다.
+                    <br />
+                        뿐만 아니라 등산 기록형 앱으로 산 북마크, 등산 기록 저장, 불러오기 기능을 구현하였고, 
+                        한줄 리뷰 남기기 기능을 통해서 사람들과 산에 대한 리뷰를 공유할수 있도록 하였습니다.
+
+                    <br /></ul>
+                
+                        <li>기술스택: kotlin, coroutines, okhttp3, retrofit2, gson, glide, koin2, viewmodel, livedata</li>
+                        <ol style={{ marginTop:'30px'}}>구현 기능: 
+                            <li className="func">지역별, 난이도별 산 추천</li>
+                            <li className="func">산 검색 기능</li>
+                            <li className="func">가고싶은 산 저장</li>
+                            <li className="func">산 위치 보기</li>
+                            <li className="func">한줄 등산 리뷰 공유</li>
+                            <li className="func">나만의 등산 리뷰</li>
+                        </ol>
+                        <section style={{ marginTop: "30px"}}>
+                            <img src="hiking1.png" style={{height:'30%',marginRight:'15px' }}/>
+                            <img src="hiking2.png" style={{height:'30%', marginRight:'15px'}}/>
+                            <img src="hiking3.png" style={{height:'30%',marginRight:'15px'}}/>
+                            <img src="hiking4.png" style={{height:'30%',marginRight:'15px'}}/>
+                            <img src="hiking5.png" style={{height:'30%',marginRight:'15px'}}/>
+                            <br/>
+                            <a style={{ marginTop:'30px', marginBottom: '100px', color:"#b8f1d0"}} href="https://github.com/Jungchaeyeon/LetsGoHiking">github 프로젝트 바로가기</a>
+                        </section>
+                    </form>
+                    {/* --------------------------------------------------------------------- */}
                 <img src="milliewillie_main.png" style={{width:'50%', height:'50%'}} />
                 <form className="appintro">
                     <h1 style={{ color:"#faf1d0"}}>밀리윌리</h1>
@@ -34,24 +114,6 @@ function PortfolioPage() {
                         <source src="demo.mp4" type="video/mp4"/>
                     </video>         
                     <a style={{ marginTop: '30px', color:"#b8f1d0"}} href='https://play.google.com/store/apps/details?id=com.makeusteam.milliewillie'>플레이스토어 밀리윌리 바로가기</a>
-                </form>
-                <form className="appintro">
-                 <h1 style={{ color:"#faf1d0"}}>낙상방지 앱 </h1>
-                <ul style={{ marginTop:'30px'}}>
-                고령화의 속도는 급격하게 증가하고 있으며, 이에 따라 발생하는 안전사고 중 절반 정도를 차지하는 낙상 사고의 심각성 또한 증가하고 있습니다.
-                <br />
-                웨어러블 디바이스는 연령에 상관없이 쉽게 이용이 가능하며,
-                상시 착용이 간편한 기기로 쉬운 데이터 활용이 가능하다는 점을 기반하여
-                이를 통해 낙상사고를 인지하고, 예방하고자 하였습니다.
-                <br /></ul>
-            
-                    <li>기술스택: java, firebase, beacon-library, naviagtion, constraintlayout</li>
-                    <ol style={{ marginTop:'30px'}}>구현 기능: 
-                        <li className="func">beacon기반 위치인식</li>
-                        <li className="func">fitbit연동</li>
-                        <li className="func">로그인, 긴급연락망</li>
-                    </ol>
-                    <a style={{ marginTop:'30px', color:"#b8f1d0"}} href="https://github.com/Jungchaeyeon/FallPrevention">github 프로젝트 바로가기</a>
                 </form>
                 <form className="appintro">
                  <h1 style={{ color: "#faf1d0", marginTop:'30px'}}>그 외 토이프로젝트</h1>
